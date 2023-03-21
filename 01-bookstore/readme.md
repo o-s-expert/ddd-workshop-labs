7acfc71914cc9c7b0b50e4f93bbbf2aa58845af5
Karina Varela <kmacedovarela@gmail.com>
2023-03-20 21:37:34 -0300

    bookstore service sample, an implementation example

---
+++ 01-bookstore/readme.md
@@ -0,0 +1,3 @@
+# Action 1: Bookstore
+
+This project is a sample service modeled on top of the previously defined domain storytelling and its components. 
---
+++ 01-bookstore/src/main/java/expert/os/workshop/ddd/bookstore/Books.java
@@ -0,0 +1,10 @@
+package expert.os.workshop.ddd.bookstore;
+
+import org.jmolecules.ddd.annotation.Repository;
+
+import java.util.List;
+
+@Repository
+public interface Books {
+    List<Book> search();
+}
