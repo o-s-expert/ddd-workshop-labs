dbe32ca6239ce4c9c4c84c89912090b3f7b64ddd
Karina Varela <kmacedovarela@gmail.com>
2023-03-22 12:01:39 -0300

    Jakarta NoSQL Lab

---
+++ 04-microprofile/src/main/java/expert/os/workshop/ddd/microprofile/RestApplication.java
@@ -0,0 +1,11 @@
+package expert.os.workshop.ddd.microprofile;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class RestApplication extends Application {
+}
--- 05-jnosql/src/main/java/expert/os/workshop/ddd/jnosql/App.java
+++ 05-jnosql/src/main/java/expert/os/workshop/ddd/jnosql/App.java
@@ -9,24 +9,58 @@
  * You may elect to redistribute this code under either of these licenses.
  */
 
-package expert.os.workshop.ddd.jnosql;
+//package expert.os.workshop.ddd.jnosql;
 
 
 import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
-import jakarta.nosql.document.DocumentTemplate;
-
-import java.time.Year;
-import java.util.List;
-import java.util.UUID;
+//import jakarta.nosql.document.DocumentTemplate;
+//
+//import java.time.Year;
+//import java.util.List;
+//import java.util.UUID;
 
 
 public class App {
-
-
     public static void main(String[] args) {
 
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+            /**
+             * TODO: Create a book instance: `effective`
+             * */
+
+
+            /**
+             * TODO: Use CDI container to create `template`, an instance of `jakarta.nosql.document.DocumentTemplate`
+             * */
+
+
+            /**
+             * TODO: Using the `template` instance, use the method `insert` to save the `effective` book
+             * */
+
+
+            /**
+             * TODO: Try out the fluent API for searching data.
+             * Use the `template.select` method to search for books where title equals to "Effective Java");
+             * Store the results in a `List<Book> books;
+             * Print out the result of the search:  System.out.println("These are the books founds: " + books);
+             * */
+
+
+
+            /**
+             * TODO: Delete
+             * Using the `template`, use the method `delete` to delete instances of `Book.class` where the `title`
+             * is equals to `Effective Java`.
+             * Remind to invoke ".execute();" method to finalize the command;
+             * */
+
+
+            /**
+             * TODO: Search and List
+             * Use the `template.select` method to search and print the result like you have done previously;
+             * */
 
         }
     }
