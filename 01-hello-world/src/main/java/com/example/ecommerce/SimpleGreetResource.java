6eea927b5eae0a6a49e280527b9ea06f06e31f28
Otavio Santana <otaviopolianasantana@gmail.com>
2025-09-08 20:36:41 +0100

    fix: remove unnecessary blank lines in SimpleGreetResource.java

--- 01-hello-world/src/main/java/com/example/ecommerce/SimpleGreetResource.java
+++ 01-hello-world/src/main/java/com/example/ecommerce/SimpleGreetResource.java
@@ -30,7 +30,6 @@ public class SimpleGreetResource {
     private static final String GETS_TIMER_NAME = "allGets";
     private static final String GETS_TIMER_DESCRIPTION = "Tracks all GET operations";
     private final String message;
-
     @Inject
     public SimpleGreetResource(@ConfigProperty(name = "app.greeting") String message) {
         this.message = message;
@@ -50,8 +49,6 @@ public class SimpleGreetResource {
         return message;
     }
 
-
-    @Path("/{name}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Counted(name = PERSONALIZED_GETS_COUNTER_NAME,
